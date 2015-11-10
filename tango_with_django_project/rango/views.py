a99176338f2cfdf8010d9ddd76ca0838ec47771f
alexnas2 <alexnas2train@gmail.com>
2015-11-10 19:29:19 +0530

    Templates + TEMPLATE_PATH and Static Media + STATIC_PATH.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,21 @@
-# from django.shortcuts import render
 from django.http import  HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return  HttpResponse("Rango says WOW to the world!")
+    '''Start page view'''
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold message from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says hear about page")
+    '''About Rango page'''
+    # Some context for template
+    context_dict = {'somemessage': "Another one message (about) from the context"}
+    return render(request, 'rango/about.html', context_dict)
 
 
