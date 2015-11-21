5bfb58ab0cb951e806ee5e9334a4bb4213b8d694
alexnas2 <alexnas2train@gmail.com>
2015-11-21 22:39:36 +0530

    Models and Database Creation
    Created basic models
    Created database
    Written script for database population
    Database is populated
    Admin Interface is initially customized

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -4,20 +4,16 @@ from django.conf import  settings
 # from django.conf.urls.static import  static
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls' )),
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns(
-        'django.views.static',
-        (r'^media/(?P<path>.*)',
-         'serve',
-         {'document_root': settings.MEDIA_ROOT}),
-    )
+# if settings.DEBUG:
+#     urlpatterns += patterns(
+#         'django.views.static',
+#         (r'^media/(?P<path>.*)',
+#          'serve',
+#          {'document_root': settings.MEDIA_ROOT}),
+#     )
 
 
